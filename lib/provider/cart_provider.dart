ccfe06773ed27e207924e4aa8506a9795ca07b64
Emyeeeel <maryamielcatado@gmail.com>
2024-02-21 23:51:06 +0800

    added svg assets and created navigation bar widget

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -8,6 +8,12 @@ class CartModel extends ChangeNotifier {
     ["Abyssinian Cat", "lib/assets/cat-5.png", 75],
     ["Burmese Cat", "lib/assets/cat-6.png", 75],
     ["Russian Blue Cat", "lib/assets/cat-7.png", 60],
+    ["Persian Cat", "lib/assets/cat-2.png", 85],
+    ["Sphinx Cat", "lib/assets/cat-3.png", 60],
+    ["Bengal Cat", "lib/assets/cat-4.png", 90],
+    ["Abyssinian Cat", "lib/assets/cat-5.png", 75],
+    ["Burmese Cat", "lib/assets/cat-6.png", 75],
+    ["Russian Blue Cat", "lib/assets/cat-7.png", 60],
   ];
 
   get shopItems => _shopItems;
